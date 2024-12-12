10576440011dea3087a8ba9717603e4009edc723
yKaayo <caiokaea@gmail.com>
2024-12-12 19:52:52 -0300

    Take out the Postcss of Build

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: './src',
+  build: {
+    rollupOptions: {
+      external: ['postcss', 'autoprefixer'],
+    },
+  },
   server: {
     port: 3000
   }
