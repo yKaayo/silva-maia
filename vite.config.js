b71fb23822d6bf30f31332b961c7d1902854efe3
yKaayo <caiokaea@gmail.com>
2024-12-12 19:55:29 -0300

    Trying to Fix the Build Problem

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,6 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: './src',
-  build: {
-    rollupOptions: {
-      external: ['postcss', 'autoprefixer'],
-    },
-  },
   server: {
     port: 3000
   }
