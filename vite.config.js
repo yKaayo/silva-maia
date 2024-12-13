3c92881dd21e1cb3764334ebfee1ec6b0aad9c0a
yKaayo <caiokaea@gmail.com>
2024-12-12 23:37:11 -0300

    Deploy Fixed

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: './src',
-  server: {
-    port: 3000
-  },
+  root: 'src',
   build: {
-    outDir: 'dist'
-  }
+    outDir: '../dist',
+  },
 });
